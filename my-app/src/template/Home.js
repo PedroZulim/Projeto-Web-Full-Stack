8fca654e8a46702330083035e54657b4e8b8d5eb
Pedro Henrique de Souza Zulim <pedrozulim2@gmail.com>
2023-10-27 14:33:26 -0300

    Home

---
+++ my-app/src/template/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home() {
+    return (
+        <div className='Main'>
+            <h1>Bem vindo a aplicativo REACT SPACE X</h1>
+            <p>Este é o conteúdo principal do seu aplicativo.</p>
+        </div>
+    );
+}
+
+export default Home;

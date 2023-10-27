b56e0fbd6dd2c6e1522b9b98514ec9cf9c242fcf
Pedro Henrique de Souza Zulim <pedrozulim2@gmail.com>
2023-10-27 14:34:52 -0300

    About

---
+++ my-app/src/template/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Main() {
+    return (
+        <div className='Main'>
+            <h1>ABOUT</h1>
+            <p>ABOUT</p>
+        </div>
+    );
+}
+
+export default Main;

a50d0f1118134d5e437b58ccd5f42474393b0528
Pedro Henrique de Souza Zulim <pedrozulim2@gmail.com>
2023-10-27 14:44:11 -0300

    footer e header css

--- my-app/src/template/Footer.js
+++ my-app/src/template/Footer.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import styled from "styled-components";
 
 function Footer() {
-  return (
-    <div className='Footer'>
-      <p>Rodapé do seu aplicativo</p>
-    </div>
-  );
+    return (
+        <footer className="footer mt-auto">
+            <span className="text-body-secondary">Rodapé do seu aplicativo</span>
+        </footer>
+    );
 }
 
 export default Footer;
--- my-app/src/template/Header.js
+++ my-app/src/template/Header.js
@@ -1,30 +1,48 @@
 import React from 'react';
+import {Link} from "react-router-dom";
 
 function Header() {
   return (
-    <div className='Header'>
-      <div className='LogoHeader'>
-        <img src="Logo-Header.png" alt='Logo SerMais'/>
-      </div>
-      <div className='buttons-header'>
-        <div className='button'>
-          <button>INÍCIO</button>
+      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <div className="container-fluid">
+          <Link to="/" className="navbar-brand"> Home</Link>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <Link to="/spacex/rockets" className="nav-link"> Rockets</Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/spacex/ships" className="nav-link"> Ships</Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/" className="nav-link"> About</Link>
+              </li>
+
+            </ul>
+          </div>
         </div>
-        <div className='button'>
-          <button>SOBRE NÓS</button>
-        </div>
-        <div className='button'>
-          <button>SERVIÇOS</button>
-        </div>
-        <div className='button'>
-          <button>EQUIPE</button>
-        </div>
-        <div className='button'>
-          <button>CONTATO</button>
-        </div>
-      </div>
-    </div>
-  );
+      </nav>
+      // <div className='Header'>
+      //
+      // 	<div className='LogoHeader'>
+      // 		<img src="Logo-Header.png" alt='Logo SerMais'/>
+      // 	</div>
+      // 	<div className='buttons-header'>
+      // 		<div className='button'>
+      // 			<button><Link to="/">Home</Link></button>
+      // 		</div>
+      // 		<div className='button'>
+      // 			<button><Link to="/about">About</Link></button>
+      // 		</div>
+      // 		<div className='button'>
+      // 			<button><Link to="/spacex/ships">Ships</Link></button>
+      // 		</div>
+      //
+      // 	</div>
+      // </div>
+
+  )
+      ;
 }
 
 export default Header;

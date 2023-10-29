e9dec992577dc6548837ad68bd618c3036956469
Pedro Henrique de Souza Zulim <pedrozulim2@gmail.com>
2023-10-29 16:24:20 -0300

    Footer

--- my-app/src/rockets/ListaRockets.js
+++ my-app/src/rockets/ListaRockets.js
@@ -57,20 +57,6 @@ function ListaRockets() {
 					</tbody>
 				</table>
 			</div>
-
-			<div className='card-footer'>
-				<div className="btn-group" role="group" aria-label="Basic example">
-					<button type="button" className="btn btn-default">Primeiro</button>
-					<button type="button" className="btn btn-default">Anterior</button>
-					<button type="button" className="btn btn-default">1</button>
-					<button type="button" className="btn btn-default">2</button>
-					<button type="button" className="btn btn-default">3</button>
-					<button type="button" className="btn btn-default">Proximo</button>
-					<button type="button" className="btn btn-default">Ultimo</button>
-				</div>
-
-			</div>
-
 		</div>
 	);
 }

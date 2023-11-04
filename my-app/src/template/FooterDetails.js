499b5bd8913b6fe2ebb920a24f37665cb95824a5
PedroZulim <pedrozulim2@gmail.com>
2023-11-03 23:09:29 -0300

    Paginação ships

--- my-app/src/template/FooterDetails.js
+++ my-app/src/template/FooterDetails.js
@@ -1,17 +1,78 @@
-import React from 'react';
-import styled from "styled-components";
+import React, {useEffect, useState} from 'react';
 
-function FooterDetails() {
+function FooterDetails(props) {
+    // acoes dos botoes da pagina
+    const primeira = () => {
+        // Chama funcao no PAI para atualizar lista
+        props.atualizaLista(1);
+    }
+    const anterior = () => {
+        // Chama funcao no PAI para atualizar lista
+        props.atualizaLista(props.page - 1);
+    }
+    const pagina = (numeroPagina) => {
+        // Chama funcao no PAI para atualizar lista
+        props.atualizaLista(numeroPagina);
+    }
+    const proxima = () => {
+        // Chama funcao no PAI para atualizar lista
+        props.atualizaLista(props.page + 1);
+    }
+    const ultima = () => {
+        // Chama funcao no PAI para atualizar lista
+        props.atualizaLista(totalPagina);
+    }
+
+    const [totalPagina, setTotalPagina] = useState(0);
+    const [listaPaginas, setListaPaginas] = useState([]);
+
+    useEffect(() => {
+        // Atualiza lista de pagina
+        let totalPagina = Math.ceil( props.totalRegistro / props.limit);
+        setTotalPagina(totalPagina);
+        const listaPaginas = [];
+        for (let i = (props.page - 2); i < (props.page + 2); i++) {
+            if (i >= 0 && i < (totalPagina)) {
+                listaPaginas.push(i + 1);
+            }
+        }
+        setListaPaginas(listaPaginas);
+    }, [props.totalRegistro, props.page]);
+
+
     return (
-        <footer className="footer mt-auto">
+        <footer className="footer mt-auto" hidden={props.atualizaLista}>
             <div className="btn-group" role="group" aria-label="Basic example">
-                <button type="button" className="btn btn-default">Primeiro</button>
-                <button type="button" className="btn btn-default">Anterior</button>
-                <button type="button" className="btn btn-default">1</button>
-                <button type="button" className="btn btn-default">2</button>
-                <button type="button" className="btn btn-default">3</button>
-                <button type="button" className="btn btn-default">Proximo</button>
-                <button type="button" className="btn btn-default">Ultimo</button>
+                <button type="button" className="btn btn-default btn-outline-dark" onClick={() => {
+                    primeira()
+                }} disabled={props.page === 1}>Primeiro
+                </button>
+
+                <button type="button" className="btn btn-default btn-outline-dark" onClick={() => {
+                    anterior()
+                }} disabled={props.page === 1}>Anterior
+                </button>
+
+                {listaPaginas.map((value) => {
+                    // Botoes intermediario -2 e +2
+                    return <button key={value} type="button" className="btn btn-default btn-outline-dark" onClick={() => {
+                        pagina(value)
+                    }} disabled={props.page === value}>{value}</button>
+                })}
+
+                <button type="button" className="btn btn-default btn-outline-dark" onClick={() => {
+                    proxima()
+                }} disabled={props.page === totalPagina}>Proximo
+                </button>
+
+                <button type="button" className="btn btn-default btn-outline-dark" onClick={() => {
+                    ultima()
+                }} disabled={props.page === totalPagina}>Ultimo
+                </button>
+            </div>
+
+            <div className='float-end'>
+                Total de {props.totalRegistro} registros. {props.page} / {totalPagina} Paginas
             </div>
         </footer>
     );

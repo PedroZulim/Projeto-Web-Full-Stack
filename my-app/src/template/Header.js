bbf35b177bfc5e6f476fb4f7bf466e4362a8a637
Pedro Henrique de Souza Zulim <pedrozulim2@gmail.com>
2023-11-06 15:04:32 -0300

    Header and Deploy

--- my-app/src/template/Header.js
+++ my-app/src/template/Header.js
@@ -1,20 +1,16 @@
 import React from 'react';
 import {Link} from "react-router-dom";
-import ship from "../images/shipIcon.svg";
-import home from "../images/homeIcon.svg";
-import rocket from "../images/rocketIcon.svg";
-import about from "../images/aboutIcon.svg";
 import logoblack from "../images/SpaceX_Logo_Black.png";
 import styled from "styled-components";
-
-const StyledImage = styled.img`
-  width: 25px;
-  height: 25px;
-`;
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import { faRocket } from '@fortawesome/free-solid-svg-icons'
+import { faHome } from '@fortawesome/free-solid-svg-icons'
+import { faShip } from '@fortawesome/free-solid-svg-icons'
+import { faInfo } from '@fortawesome/free-solid-svg-icons'
 
 const StyledDiv = styled.div`
   display: flex;
-  alignItems: center;
+  align-items: center;
 `;
 
 function Header() {
@@ -22,36 +18,35 @@ function Header() {
       <nav className="navbar navbar-expand-lg bg-body-tertiary">
         <div className="container-fluid">
           <StyledDiv>
-            <StyledImage src={home} className="me-2 mt-2"/>
+            <FontAwesomeIcon icon={faHome} size={70}/>
             <Link to="/" className="navbar-brand"> Home</Link>
           </StyledDiv>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
                 <StyledDiv className="ms-2">
-                  <StyledImage className="mt-2" src={rocket}/>
+                  <FontAwesomeIcon icon={faRocket} size={70}/>
                   <Link to="/spacex/rockets" className="nav-link"> Rockets</Link>
                 </StyledDiv>
               </li>
               <li className="nav-item">
                 <StyledDiv className="ms-2">
-                  <StyledImage className="mt-2" src={ship}/>
+                  <FontAwesomeIcon icon={faShip} size={70}/>
                   <Link to="/spacex/ships" className="nav-link">Ships</Link>
                 </StyledDiv>
               </li>
               <li className="nav-item">
                 <StyledDiv className="ms-2">
-                  <StyledImage className="mt-2" src={about}/>
+                  <FontAwesomeIcon icon={faInfo} size={70}/>
                   <Link to="/About" className="nav-link"> About</Link>
                 </StyledDiv>
               </li>
             </ul>
           </div>
-          <img src={logoblack} alt="Descrição da imagem" width="250" height="40" />
+          <img src={logoblack} alt="Descrição da imagem" width="250" height="40"/>
         </div>
       </nav>
   )
-      ;
 }
 
 export default Header;

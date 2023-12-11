8aad337a6a2760be024c011fbcf7c05448eb6e2f
PedroZulim <pedrozulim2@gmail.com>
2023-12-11 08:14:15 -0300

    Login

--- my-app/src/template/Login.js
+++ my-app/src/template/Login.js
@@ -5,8 +5,7 @@ import {useRef} from "react";
 
 const StyledForm = styled.form`
   align-items: center;
-  padding-top: 40px;
-  padding-bottom: 40px;
+  padding-bottom: 20px;
 `;
 
 const StyledMain = styled.main`
@@ -18,8 +17,40 @@ const StyledMain = styled.main`
 
 function Login() {
 
-    const passwordInputRef  = useRef('');
+    const passwordInputRef = useRef('');
     const emailInputRef = useRef('');
+
+    const signin = () => {
+        const objLogin = {email: emailInputRef.current.value, password: passwordInputRef.current.value};
+        // Call the PutAPI hook with the appropriate URL and object
+        const requestOptions = {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(objLogin)
+        };
+        // Simple DELETE request with fetch
+        fetch('https://localhost:5000/api/login/', requestOptions)
+    .then((res) => res.json())
+            .then((data) => {
+                alert(data.message);
+            });
+    }
+    const register = () => {
+        const objLogin = {email: emailInputRef.current.value, password: passwordInputRef.current.value};
+        // Call the PutAPI hook with the appropriate URL and object
+        const requestOptions = {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(objLogin)
+        };
+        // Simple DELETE request with fetch
+        fetch('https://localhost:5000/api/register/', requestOptions)
+    .then((res) => res.json())
+            .then((data) => {
+                alert(data.message);
+            });
+    }
+
 
     return (
         <div className="text-center">
@@ -42,7 +73,8 @@ function Login() {
                             <input type="checkbox" value="remember-me"/> Remember me
                         </label>
                     </div>
-                    <button className="w-100 btn btn-lg btn-primary bg-black border-black" type="submit">Sign in</button>
+                    <button className="w-100 btn btn-lg btn-primary bg-dark border-black mb-2" onClick={() => signin()}>Sign in</button>
+                    <button className="w-75 btn  btn-secondary bg-dark border-black" onClick={() => register()}>Register</button>
                 </StyledForm>
             </StyledMain>
         </div>

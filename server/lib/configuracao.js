f039490c5d6b5c3cf1298814776b1f24f438daeb
Pedro Henrique de Souza Zulim <pedrozulim2@gmail.com>
2023-12-12 21:25:31 -0300

    winston

--- server/lib/configuracao.js
+++ server/lib/configuracao.js
@@ -27,8 +27,8 @@ const config= {
 		format: winston.format.json(),
 		defaultMeta: {service: 'user-service'},
 		transports: [
-			new winston.transports.File({filename: '../logs/error.log', level: 'error'}),
-			new winston.transports.File({filename: '../logs/combined.log'}),
+			new winston.transports.File({filename: './logs/error.log', level: 'error'}),
+			new winston.transports.File({filename: './logs/combined.log'}),
 		],
 	}),
 	jwtSecretKey: "RANDOM-TOKENx",
